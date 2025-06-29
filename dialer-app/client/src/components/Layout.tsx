f121450a667cf843e4740166f45e35f3e768bb6a
Link P <linkp@Links-Mac-mini.local>
2025-06-29 00:59:12 -0400

    feat(client): hide dialer component from all pages

--- dialer-app/client/src/components/Layout.tsx
+++ dialer-app/client/src/components/Layout.tsx
@@ -43,7 +43,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         {children}
       </div>
       {/* Add Dialer component to all pages */}
-      <Dialer />
+      {/* <Dialer /> */}
     </div>
   );
 }
